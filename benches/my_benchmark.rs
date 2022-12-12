75a74efb24c87fbdc650392da1c788502ae2c8ec
Ryan P. Brewster <RyanPBrewster@gmail.com>
2022-12-12 00:37:33 -0500

    down enormously

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -5,7 +5,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fill_1000", |b| {
         b.iter(|| {
             let mut file = tempfile::tempfile().unwrap();
-            let mut w = Writer::new(&mut file);
+            let mut w = Writer::new(&mut file).unwrap();
             for i in 0..1_1000 {
                 let key = format!("key-{}", i);
                 let value = format!("value-{}", i);
@@ -17,15 +17,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("read_1000_of_1000", |b| {
         let mut file = tempfile::tempfile().unwrap();
         {
-            let mut w = Writer::new(&mut file);
+            let mut w = Writer::new(&mut file).unwrap();
             for i in 0..1_000 {
                 let key = format!("key-{}", i);
                 let value = format!("value-{}", i);
                 w.append(key.as_bytes(), value.as_bytes()).unwrap();
             }
+            w.finish().unwrap();
         }
         b.iter(|| {
-            let mut r = Reader::new(&mut file);
+            let mut r = Reader::new(&mut file).unwrap();
             for i in 0..1_000 {
                 let key = format!("key-{}", i);
                 black_box(r.read(key.as_bytes()).unwrap());
@@ -36,15 +37,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("read_1000_nonexistent", |b| {
         let mut file = tempfile::tempfile().unwrap();
         {
-            let mut w = Writer::new(&mut file);
+            let mut w = Writer::new(&mut file).unwrap();
             for i in 0..1_000 {
                 let key = format!("key-{}", i);
                 let value = format!("value-{}", i);
                 w.append(key.as_bytes(), value.as_bytes()).unwrap();
             }
+            w.finish().unwrap();
         }
         b.iter(|| {
-            let mut r = Reader::new(&mut file);
+            let mut r = Reader::new(&mut file).unwrap();
             for i in 0..1_000 {
                 let key = format!("garbage-{}", i);
                 let _ = black_box(r.read(key.as_bytes()));
