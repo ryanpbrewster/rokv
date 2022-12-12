35ca0f06736a18477015d2c7c6d3dd54a1a46c85
Ryan P. Brewster <RyanPBrewster@gmail.com>
2022-12-12 00:35:21 -0500

    construct and persist a table

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,118 @@
 use std::{
     fs::File,
-    io::{self, BufWriter, Read, Seek, SeekFrom, Write, BufReader},
+    io::{BufWriter, Read, Seek, SeekFrom, Write, BufReader},
 };
 
-use byteorder::ByteOrder;
+use byteorder::{ReadBytesExt, LittleEndian, WriteBytesExt};
 
 pub struct Writer<'a> {
     file: BufWriter<&'a mut File>,
+    offset: u32,
+    log: Vec<Entry>,
+}
+struct Entry {
+    key_hash: u32,
+    offset: u32,
 }
 impl<'a> Writer<'a> {
-    pub fn new(file: &'a mut File) -> Self {
-        Writer {
+    pub fn new(file: &'a mut File) -> anyhow::Result<Self> {
+        // Leave room for the header: a pair of u32 values indicating the offset + length of the footer table.
+        file.seek(SeekFrom::Start(8))?;
+        Ok(Writer {
             file: BufWriter::new(file),
-        }
+            offset: 8,
+            log: Vec::new(),
+        })
     }
     pub fn append(&mut self, key: &[u8], value: &[u8]) -> anyhow::Result<()> {
-        let mut buf = [0; 4];
-
-        byteorder::LittleEndian::write_u32(&mut buf, key.len() as u32);
-        self.file.write_all(&buf)?;
+        self.log.push(Entry{ key_hash: cdb_hash(key), offset: self.offset });
+        self.offset += key.len() as u32 + value.len() as u32 + 8;
+        
+        self.file.write_u32::<LittleEndian>(key.len() as u32)?;
         self.file.write_all(key)?;
 
-        byteorder::LittleEndian::write_u32(&mut buf, value.len() as u32);
-        self.file.write_all(&buf)?;
+        self.file.write_u32::<LittleEndian>(value.len() as u32)?;
         self.file.write_all(value)?;
+
+        Ok(())
+    }
+    pub fn finish(mut self) -> anyhow::Result<()> {
+        // Compute a hashtable. We're using linear probing, so make sure the
+        // load factor is not too high. We'll go with 0.5 for now.
+        let mut table: Vec<u32> = vec![0; 2 * self.log.len()];
+        for entry in self.log {
+            let mut slot = entry.key_hash as usize % table.len();
+            // In the case of collisions, perform linear probing to find an empty slot.
+            while table[slot] > 0 {
+                slot = (slot + 1) % table.len();
+            }
+            table[slot] = entry.offset;
+        }
+
+        // Write the hashtable.
+        for &offset in &table {
+            self.file.write_u32::<LittleEndian>(offset)?;
+        }
+        
+        // Write the offset for the hashtable.
+        self.file.seek(SeekFrom::Start(0))?;
+        self.file.write_u32::<LittleEndian>(self.offset)?;
+        self.file.write_u32::<LittleEndian>(table.len() as u32)?;
+        self.file.flush()?;
 
         Ok(())
     }
 }
 
+
+// A simple hash function copied from CDB
+fn cdb_hash(key: &[u8]) -> u32 {
+    let mut h: u32 = 5381;
+    for &c in key {
+        h = h.wrapping_mul(33) ^ c as u32;
+    }
+    h
+}
+
 pub struct Reader<'a> {
     file: BufReader<&'a mut File>,
+    table: Vec<u32>,
 }
 impl<'a> Reader<'a> {
-    pub fn new(file: &'a mut File) -> Self {
-        Reader { file: BufReader::new(file) }
+    pub fn new(file: &'a mut File) -> anyhow::Result<Self> {
+        file.seek(SeekFrom::Start(0))?;
+        let mut file = BufReader::new(file);
+        let table_offset = file.read_u32::<LittleEndian>()?;
+        let table_len = file.read_u32::<LittleEndian>()? as usize;
+        file.seek(SeekFrom::Start(table_offset as u64))?;
+
+        let mut table = vec![0; table_len];
+        for slot in table.iter_mut() {
+            *slot = file.read_u32::<LittleEndian>()?;
+        }
+
+        Ok(Reader { file, table })
     }
-    pub fn read(&mut self, key: &[u8]) -> io::Result<Vec<u8>> {
-        let mut buf = [0; 4];
+    pub fn read(&mut self, key: &[u8]) -> anyhow::Result<Option<Vec<u8>>> {
+        let mut slot = cdb_hash(key) as usize % self.table.len();
+        while self.table[slot] > 0 {
+            self.file.seek(SeekFrom::Start(self.table[slot] as u64))?;
 
-        self.file.seek(SeekFrom::Start(0))?;
-        loop {
-            self.file.read_exact(&mut buf)?;
-            let key_len = byteorder::LittleEndian::read_u32(&buf) as usize;
-
-            let mut key_buf = vec![0; key_len];
+            let key_len = self.file.read_u32::<LittleEndian>()?;
+            let mut key_buf = vec![0; key_len as usize];
             self.file.read_exact(&mut key_buf)?;
 
-            self.file.read_exact(&mut buf)?;
-            let value_len = byteorder::LittleEndian::read_u32(&buf) as usize;
-
             if key_buf != key {
-                self.file.seek_relative(value_len as i64)?;
+                slot = (slot + 1) % self.table.len();
                 continue;
             }
 
-            let mut value_buf = vec![0; value_len];
+            let value_len = self.file.read_u32::<LittleEndian>()?;
+            let mut value_buf = vec![0; value_len as usize];
             self.file.read_exact(&mut value_buf)?;
-            return Ok(value_buf);
+            return Ok(Some(value_buf));
         }
+        Ok(None)
     }
 }
 
@@ -71,13 +125,14 @@ mod test {
         let mut file = tempfile::tempfile()?;
 
         {
-            let mut w = Writer::new(&mut file);
+            let mut w = Writer::new(&mut file)?;
             w.append(b"hello", b"world")?;
+            w.finish()?;
         }
 
-        let mut r = Reader::new(&mut file);
-        assert_eq!(r.read(b"hello")?, b"world");
-        assert!(r.read(b"foo").is_err(), "expected reading foo to error out");
+        let mut r = Reader::new(&mut file)?;
+        assert_eq!(r.read(b"hello")?, Some(b"world".to_vec()));
+        assert_eq!(r.read(b"foo")?, None);
 
         Ok(())
     }
