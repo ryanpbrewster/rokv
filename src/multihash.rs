deee3162edf266241d4aaa075e1ce25fbf52d434
Ryan P. Brewster <RyanPBrewster@gmail.com>
2022-12-27 20:12:04 -0500

    run multiple trials

--- src/multihash.rs
+++ src/multihash.rs
@@ -44,12 +44,13 @@ impl MultihashTable {
         for _ in 0 .. MAX_ATTEMPTS {
             if let Some(prev) = self.slots[loc].replace(cur) {
                 cur = prev;
-                loc = loop {
+                for _ in 0 .. MAX_ATTEMPTS {
                     let h = farmhash::hash32_with_seed(cur.as_bytes(), self.prng.gen_range(0..self.num_hashes)) as usize;
                     let base = (h % self.num_slots) * self.slot_capacity;
                     let candidate = base + self.prng.gen_range(0 .. self.slot_capacity);
                     if candidate != loc {
-                        break candidate;
+                        loc = candidate;
+                        break;
                     }
                 }
             } else {
@@ -73,7 +74,6 @@ mod test {
             let mut h = MultihashTable::new(num_hashes, slot_capacity, BASE_CAPACITY);
             let mut count = 0;
             while h.insert(format!("trial-{}-key-{}", trial, count)) {
-                println!("inserted trial-{}-key-{}", trial, count);
                 count += 1;
             }
             total += count as f64 / (BASE_CAPACITY * slot_capacity) as f64;
@@ -84,41 +84,44 @@ mod test {
 
     #[test]
     fn achievable_load_golden_test() {
-        assert_eq!(achievable_load(2, 1), 0.564);
-        assert_eq!(achievable_load(3, 1), 0.832);
-        assert_eq!(achievable_load(4, 1), 0.853);
-        assert_eq!(achievable_load(5, 1), 0.892);
-        assert_eq!(achievable_load(6, 1), 0.892);
-        assert_eq!(achievable_load(7, 1), 0.920);
-        assert_eq!(achievable_load(8, 1), 0.919);
+        assert_eq!(achievable_load(2, 1), 0.547);
+        assert_eq!(achievable_load(3, 1), 0.815);
+        assert_eq!(achievable_load(4, 1), 0.873);
+        assert_eq!(achievable_load(5, 1), 0.896);
+        assert_eq!(achievable_load(6, 1), 0.910);
+        assert_eq!(achievable_load(7, 1), 0.917);
+        assert_eq!(achievable_load(8, 1), 0.921);
 
-        assert_eq!(achievable_load(2, 2), 0.785);
-        assert_eq!(achievable_load(3, 2), 0.858);
-        assert_eq!(achievable_load(4, 2), 0.920);
-        assert_eq!(achievable_load(5, 2), 0.914);
-        assert_eq!(achievable_load(6, 2), 0.919);
-        assert_eq!(achievable_load(7, 2), 0.927);
-        assert_eq!(achievable_load(8, 2), 0.895);
+        assert_eq!(achievable_load(2, 2), 0.763);
+        assert_eq!(achievable_load(3, 2), 0.871);
+        assert_eq!(achievable_load(4, 2), 0.896);
+        assert_eq!(achievable_load(5, 2), 0.911);
+        assert_eq!(achievable_load(6, 2), 0.920);
+        assert_eq!(achievable_load(7, 2), 0.923);
+        assert_eq!(achievable_load(8, 2), 0.933);
 
-        assert_eq!(achievable_load(2, 3), 0.806);
-        assert_eq!(achievable_load(3, 3), 0.914);
-        assert_eq!(achievable_load(4, 3), 0.885);
-        assert_eq!(achievable_load(5, 3), 0.931);
-        assert_eq!(achievable_load(6, 3), 0.905);
-        assert_eq!(achievable_load(7, 3), 0.945);
-        assert_eq!(achievable_load(8, 3), 0.928);
+        assert_eq!(achievable_load(2, 3), 0.814);
+        assert_eq!(achievable_load(3, 3), 0.883);
+        assert_eq!(achievable_load(4, 3), 0.901);
+        assert_eq!(achievable_load(5, 3), 0.913);
+        assert_eq!(achievable_load(6, 3), 0.918);
+        assert_eq!(achievable_load(7, 3), 0.931);
+        assert_eq!(achievable_load(8, 3), 0.934);
 
-        assert_eq!(achievable_load(2, 4), 0.891);
-        assert_eq!(achievable_load(3, 4), 0.880);
-        assert_eq!(achievable_load(4, 4), 0.920);
-        assert_eq!(achievable_load(5, 4), 0.943);
-        assert_eq!(achievable_load(6, 4), 0.942);
-        assert_eq!(achievable_load(7, 4), 0.913);
-        assert_eq!(achievable_load(8, 4), 0.940);
+        assert_eq!(achievable_load(2, 4), 0.843);
+        assert_eq!(achievable_load(3, 4), 0.892);
+        assert_eq!(achievable_load(4, 4), 0.912);
+        assert_eq!(achievable_load(5, 4), 0.919);
+        assert_eq!(achievable_load(6, 4), 0.921);
+        assert_eq!(achievable_load(7, 4), 0.929);
+        assert_eq!(achievable_load(8, 4), 0.933);
     }
 
     #[test]
     fn farmhash_does_not_explode() {
+        // This test fails in debug mode for farmhashv1.15, but should pass in
+        // release mode, and it should pass once
+        // https://github.com/seiflotfy/rust-farmhash/pull/14 is merged.
         let _ = farmhash::hash32_with_seed("trial-0-key-27".as_bytes(), 1);
     }
 }
